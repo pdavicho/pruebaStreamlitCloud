a0803afda03af7df5b7f8bf309603f7aff779e1e
pdavicho <pablodavid218@gmail.com>
2021-11-14 18:03:16 -0500

    Prueba 2

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.text('Hello world')
 x = st.slider('x') 
 st.write(x, 'squared is', x * x)
+
+option = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone'))
+st.write('You selected:', option)
