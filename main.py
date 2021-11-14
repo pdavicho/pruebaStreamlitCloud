c64803f5ea39628a85a81b9556e7c94c9df97089
pdavicho <pablodavid218@gmail.com>
2021-11-14 18:41:26 -0500

    Prueba 3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.text('Hello world')
 x = st.slider('x') 
@@ -6,3 +7,6 @@ st.write(x, 'squared is', x * x)
 
 option = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone'))
 st.write('You selected:', option)
+
+hoja = pd.read_csv('prueba.csv')
+hoja.head()
